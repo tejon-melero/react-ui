fac88478fea14bc01401e07570108d850e2c1c92
Alex Russell <alex.russell@cotidia.com>
2016-10-31 09:43:58 +0000

    Update MultipleSelect so that it passes full objects rather than simple values around. This is specifically so that the 'prepopulated' values can be shown when the options don't necessarily contain the selected values. This may need a rethink, as passing objects around goes against the rest of React UI.

--- src/MultipleSelect.js
+++ src/MultipleSelect.js
@@ -4,6 +4,8 @@ import { formControlPropTypes, hasOptionsPropTypes, focussablePropTypes } from '
 
 import Select from './Select'
 
+const valueMapper = (item) => item.value
+
 export default class MultipleSelect extends Component {
     static propTypes = {
         ...formControlPropTypes,
@@ -20,84 +22,75 @@ export default class MultipleSelect extends Component {
         searchOptions: PropTypes.func,
         searchingPlaceholder: PropTypes.string,
         type: PropTypes.string,
-        value: PropTypes.arrayOf(PropTypes.any).isRequired,
+        value: () => {},
+        values: PropTypes.arrayOf(PropTypes.any).isRequired,
     }
 
     /**
-     * Add value(s) to the currently-selected values.
+     * Add or remove a value to the currently-selected values.
      *
-     * @returns void
+     * @param {Object} data - The data on which option was selected.
+     *
+     * @returns {void}
      */
     _updateValue = (data) => {
-        let newValue = [ ...this.props.value ]
+        const option = this.props.options.filter((item) => (item.value === data[this.props.name]))[0]
 
-        for (const key in data) {
-            if (newValue.includes(data[key])) {
-                newValue = newValue.filter((item) => item !== data[key])
-            } else {
-                newValue.push(data[key])
-            }
+        if (this.props.values.map(valueMapper).includes(option.value)) {
+            this._removeOption(option)
+        } else {
+            this.props.updateValue({ [this.props.name]: [ ...this.props.values, option ] })
         }
-
-        this.props.updateValue({ [this.props.name]: newValue })
-    }
-
-    /**
-     * Get the list of options we have available for selection.
-     *
-     * @returns Object[] The options that aren't selected.
-     */
-    _getAvailableOptions() {
-        return this.props.options.filter((item) => (! this.props.value.includes(item.value)))
-    }
-
-    /**
-     * Get the list of options that are currently selected.
-     *
-     * @returns Object[] The currently-selected options.
-     */
-    _getSelectedOptions() {
-        return this.props.options.filter((item) => this.props.value.includes(item.value))
-    }
-
-    _getFormattedOptions() {
-        return this.props.options.map((item) => {
-            if (this.props.value.includes(item.value)) {
-                item.richLabel = (
-                    <span>{ '✓ ' }{ item.label }</span>
-                )
-
-                item.classes = `${ item.classes || '' } control-select__option--focused`
-            }
-
-            return item
-        })
     }
 
     /**
      * Remove the given option from our selected options.
      *
-     * @returns void
+     * @param {Object} option - The option to be removed.
+     *
+     * @returns {void}
      */
-    _removeOption(option, e) {
-        e.preventDefault()
-
+    _removeOption(option) {
         this.props.updateValue({
-            [this.props.name]: this.props.value.filter((item) => (item !== option.value)),
+            [this.props.name]: this.props.values.filter((item) => (item.value !== option.value)),
+        })
+    }
+
+    /**
+     * Get the options to pass into the composed Seelct component, but preformat the options first.
+     *
+     * @returns {Object[]} The list of options.
+     */
+    _getFormattedOptions() {
+        const values = this.props.values.map(valueMapper)
+
+        return this.props.options.map((item) => {
+            // If our values contain the option in question, then create a rich label from the
+            // plain label that indicates that it is selected, and also add the focused class to the
+            // containing div.
+            if (values.includes(item.value)) {
+                return {
+                    ...item,
+                    richLabel: (<span>{ '✓ ' }{ item.label }</span>),
+                    classes: `${ item.classes || '' } control-select__option--focused`,
+                }
+            }
+
+            // If the option is not selected, just return it as-is
+            return item
         })
     }
 
     render() {
         let selectedItems = null
 
-        if (this.props.value.length) {
-            selectedItems = this._getSelectedOptions().map((option) => (
+        if (this.props.values.length) {
+            selectedItems = this.props.values.map((option) => (
                 <div className="tag" key={ option.value }>
                     { option.label }
                     { ' ' }
                     <a
                         className="tag__clear"
-                        href="#"
                         onClick={ this._removeOption.bind(this, option) }
                     >
                         { '×' }
@@ -127,7 +120,7 @@ export default class MultipleSelect extends Component {
                     searchingPlaceholder={ this.props.searchingPlaceholder }
                     type={ this.props.type }
                     updateValue={ this._updateValue }
-                    value={ this.props.value }
+                    value={ this.props.values.map(valueMapper) }
                 />
 
                 { selectedItems }
