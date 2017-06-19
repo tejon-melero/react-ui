735349813dc9991359531c027a613b0fb0894a4e
Alex Russell <alex.russell@cotidia.com>
2017-06-19 11:02:28 +0100

    Add space between checkbox and its label

--- src/Checkbox.js
+++ src/Checkbox.js
@@ -55,7 +55,7 @@ export default class Checkbox extends Component {
                         type="checkbox"
                         value={ 1 }
                     />
-
+                    { ' ' }
                     { this.props.label }
                 </Label>
                 <SubHelp help={ this.props.subHelp }/>
