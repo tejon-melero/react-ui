24628e4354eeba2fae0194841c9c9dbf21853f0e
Alex Russell <alex.russell@cotidia.com>
2017-08-08 10:55:45 +0100

    Make FieldError a functional component just for fun.

--- src/Utils/FieldError.js
+++ src/Utils/FieldError.js
@@ -1,42 +1,42 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import classnames from 'classnames'
 
 // TODO: work this into styles, as padding and margin shouldn't be in the code, see Cotidia/react-ui#1
 const BOTTOM_MARGIN = 6
 
-export default class FieldError extends Component {
-    static propTypes = {
-        error: PropTypes.string,
-        on: PropTypes.bool,
-        position: PropTypes.number,
+const FieldError = ({ error, on, position }) => {
+    if (! error) {
+        return null
     }
 
-    static defaultProps = {
-        on: false,
+    const errorClasses = classnames({
+        'form__error': true,
+        'form__error--tooltip': true,
+        'form__error--tooltip-on': on,
+    })
+
+    const errorStyle = {}
+
+    if (position) {
+        errorStyle.bottom = position + BOTTOM_MARGIN
     }
 
-    render() {
-        if (! this.props.error) {
-            return null
-        }
-
-        const errorClasses = classnames({
-            'form__error': true,
-            'form__error--tooltip': true,
-            'form__error--tooltip-on': this.props.on,
-        })
-
-        const errorStyle = {}
-
-        if (this.props.position) {
-            errorStyle.bottom = this.props.position + BOTTOM_MARGIN
-        }
-
-        return (
-            <div className={ errorClasses } style={ errorStyle }>
-                { this.props.error }
-            </div>
-        )
-    }
+    return (
+        <div className={ errorClasses } style={ errorStyle }>
+            { error }
+        </div>
+    )
 }
+
+FieldError.propTypes = {
+    error: PropTypes.string,
+    on: PropTypes.bool,
+    position: PropTypes.number,
+}
+
+FieldError.defaultProps = {
+    on: false,
+}
+
+export default FieldError
